83553b1e3bf5457f99547b414332106eaf01b292
lshhong <464008968@qq.com>
2018-07-20 11:30:10 +0800

    ''

---
+++ src/components/nav-footer/nav-footer.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class NavFooter extends Component {
+  render(){
+    return(
+      <div>NavFooter</div>
+    )
+  }
+}
---
+++ src/containers/laoban/laoban.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class Laoban extends Component {
+  render(){
+    return(
+      <div>Laoban</div>
+    )
+  }
+}
